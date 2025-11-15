58013c18bba227ea359af84d31411b9ffd2df967
Sudhanshu <srsrivastava1926@gmail.com>
2025-11-15 15:18:59 +0530

    Set up Consumer

---
+++ consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group" )
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+
+}
